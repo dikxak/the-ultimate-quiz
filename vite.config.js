876c96b1099f547be2f29acf4aeea08ff4fe8c65
dikxak <dikshak@truemark.com.np>
2023-07-08 20:47:59 +0545

    initialize project for development

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: "3000",
+  },
+  resolve: {
+    alias: [
+      { find: "apis", replacement: "/src/apis" },
+      { find: "assets", replacement: "/src/assets" },
+      { find: "components", replacement: "/src/components" },
+      { find: "constants", replacement: "/src/constants" },
+      { find: "pages", replacement: "/src/pages" },
+      { find: "utils", replacement: "/src/utils" },
+    ],
   },
 });
